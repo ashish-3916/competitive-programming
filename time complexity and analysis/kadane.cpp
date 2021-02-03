9c337c13e2ddf1fb2364af247a517658db6ecae2
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2021-02-03 21:21:05 +0530

    Update kadane.cpp

--- time complexity and analysis/kadane.cpp
+++ time complexity and analysis/kadane.cpp
@@ -1,6 +1,24 @@
+
+/// negative excluded
+int maxSubarraySum(int arr[], int n)
+{
+  int curr =0 ;
+  int best=0;  // initilize best to int_min/long min if neg are included
+  
+  for(int i=0;i<n;i++)
+  {
+    curr+=arr[i];
+    if(curr>best)
+    best=curr;
+    
+    if(curr<0)curr=0;
+  }
+  return best;
+}
+
 //negative included
 
-int maxSubarraySum(int arr[], int n){
+int maxSubarraySum(int arr[], int n){  // this gave WA in one CC submission , f pepcoding
     int ans= arr[0];
     int curr = arr[0];
     for(int i=1 ; i< n ;i++)
@@ -15,20 +33,5 @@ int maxSubarraySum(int arr[], int n){
     return ans;
  }
 
-/// negative excluded
 
-int maxSubarraySum(int arr[], int n)
-{
-  int curr =0 ;
-  int best=0;
-  
-  for(int i=0;i<n;i++)
-  {
-    curr+=arr[i];
-    if(curr>best)
-    best=curr;
-    
-    if(curr<0)curr=0;
-  }
-  return best;
-}
+
