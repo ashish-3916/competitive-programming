1e5f1115c8cba039300b0fac12f5c0a9505b91d9
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2021-02-18 17:12:10 +0530

    Create remove duplicates recursively.cpp

---
+++ advance recursion/remove duplicates recursively.cpp
@@ -0,0 +1,16 @@
+void removeConsecutiveDuplicates(char *arr) {
+	
+    if(arr[0]=='\0')
+        return ;
+    
+    if(arr[0]==arr[1])
+    {
+        for(int i = 0 ; arr[i]!='\0' ; i++)
+            arr[i]=arr[i+1];
+         removeConsecutiveDuplicates(arr);
+    }
+    else
+     removeConsecutiveDuplicates(arr+1);
+
+
+}
