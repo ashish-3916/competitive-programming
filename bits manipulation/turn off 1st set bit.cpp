50862050e879975029af58e10cb948a68bb5cabd
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2021-02-21 20:48:31 +0530

    important

--- bits manipulation/turn off 1st set bit.cpp
+++ bits manipulation/turn off 1st set bit.cpp
@@ -1,7 +1,7 @@
 int turnOffFirstSetBit(int n){
  
     return n ^ ( n&(-1*n) );
-    
+  // return n & (n-1) ;  
 }
 
 
@@ -16,4 +16,13 @@ n&-n = 0000100   ( first set bit )
 
 ^  --> flips that bit
 so n^(n&-n) = 0001000 answer
+*/
+
+
+/*
+ n=12   n-1=11
+  12=00001100
+  11=00001011
+  & =00001000
+
 */
