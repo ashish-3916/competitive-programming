7dc03add52de744f74b12590f825506dcab80688
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2021-04-02 14:54:47 +0530

    Update SCC - kosaraju algo [E002].cpp

--- GRAPH PRACTICE/SCC - kosaraju algo [E002].cpp
+++ GRAPH PRACTICE/SCC - kosaraju algo [E002].cpp
@@ -1,106 +1,116 @@
-/*
-  https://www.codechef.com/problems/CHEFRRUN/
-	
-	const int N  = 1e5 + 1 ;
-	vector<ll> graph[N];
-	vector<ll> transpose[N];
-	vector<ll> out_time(N);
-	vector<ll>visited(N);
-	vector<ll>skips(N);
-	vector<ll>nodes;
-	ll n , x;
-	ll idx = 0 ;
-  
-	void dfs1(ll sv)
+//https://www.codechef.com/problems/CHEFRRUN
+const int N  = 1e5 + 1 ;
+vector<ll> graph[N];
+vector<ll> transpose[N];
+vector<ll>visited(N);
+vector<ll>skips(N);
+vector<ll>scc;
+vector<ll>out_time;
+ll n , x;
+
+void dfs(ll sv)
+{
+	visited[sv] = 1;
+
+	for (ll child :  graph[sv])
+		if (!visited[child])
+			dfs(child);
+
+	out_time.push_back(sv);
+
+}
+void dfs1(ll sv)
+{
+	visited[sv] = 1;
+
+	for (ll child :  transpose[sv])
+		if (!visited[child])
+			dfs1(child);
+
+	scc.push_back(sv);
+}
+
+void solve()
+{
+
+	scc.clear(); out_time.clear();
+	for (ll i = 0; i < N ; i++)
 	{
-		visited[sv] = 1;
-		nodes.push_back(sv);
-
-		for (ll child :  transpose[sv])
-			if (!visited[child])
-				dfs1(child);
-
+		graph[i].clear();
+		transpose[i].clear();
+		visited[i] = 0;
+		skips[i] = 0;
 	}
-	void dfs(ll sv)
+
+	cin >> n;
+	for (ll i = 0 ; i < n ; i++)
 	{
-		visited[sv] = 1;
+		cin >> x;
+		skips[i] = x;
 
-		for (ll child :  graph[sv])
-			if (!visited[child])
-				dfs(child);
+		ll a  = i ;
+		ll b = (i + x + 1) % n;
 
-		out_time[idx++] = sv;
+		if (a == b)continue ; // self loop , will handle later, test case 2
 
+		graph[a].push_back(b);
+		transpose[b].push_back(a);
 	}
-  
-	void solve()
+
+	for (ll i = 0 ; i < n ; i++)
 	{
-		for (ll i = 0; i < N ; i++)
+		if (!visited[i])
+			dfs(i);
+	}
+
+	for (ll i = 0 ; i < n ; i++)
+	{
+		visited[i] = 0;
+	}
+
+
+	reverse(all(out_time));
+
+	ll ans = 0 ;
+	for (int i : out_time)
+	{
+		if (!visited[i])
 		{
-			graph[i].clear();
-			transpose[i].clear();
-			out_time[i] = 0;
-			visited[i] = 0;
-			skips[i] = 0;
-		}
+			scc.clear();
+			dfs1(i);
 
-		cin >> n;
-		for (ll i = 0 ; i < n ; i++)
-		{
-			cin >> x;
-			skips[i] = x;
-
-			ll a  = i ;
-			ll b = (i + x + 1) % n;
-
-			if (a == b)continue ; // self loop , will handle later, test case 2
-
-			graph[a].push_back(b);
-			transpose[b].push_back(a);
-		}
-
-		for (ll i = 0 ; i < n ; i++)
-		{
-			if (!visited[i])
-				dfs(i);
-		}
-
-		for (ll i = 0 ; i < n ; i++)
-		{
-			visited[i] = 0;
-		}
-		vector<vector<ll>>component;
-		for (ll i = idx - 1 ; i >= 0 ; i--)
-		{
-			if (!visited[out_time[i]])
-			{
-				nodes.clear();
-				dfs1(out_time[i]);
-				component.push_back(nodes);
-			}
-		}
-
-		//debug(component);
-		ll ans = 0 ;
-		for (auto v : component)
-		{
-			ll s = v.size();
+			ll s = scc.size();
 			if (s > 1)
 				ans += s;
 
-			else /// check for self loop
+			else if (s == 1) /// check for self loop
 			{
-				ll a  = v[0] ;
-				ll b = (v[0] + skips[v[0]] + 1) % n;
+				ll a  = scc[0] ;
+				ll b = (scc[0] + skips[scc[0]] + 1) % n;
 				if (a == b) // if present
 					ans++;
 			}
-		}
 
-		cout << ans << endl;
+		}
 	}
 
+	cout << ans << endl;
+}
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("error.txt", "w", stderr);
+	freopen("output.txt", "w", stdout);
+#endif
 
+	int t;
+	cin >> t;
+	while (t--)
+	{solve();}
+	return 0;
 
+}
 
-*/
